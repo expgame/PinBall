fe0696e8eaa2a5e566d748e24f9a5866945f7f44
expgame <exprobot@gmail.com>
2018-06-17 18:42:41 +0900

    ピンボール課題をコミット

---
+++ Assets/BallController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour {
+	//ボールが見える可能性のあるz軸の最大値
+	private float visiblePosZ = -6.5f;
+
+	//ゲームオーバを表示するテキスト
+	private GameObject gameoverText;
+
+	//スコア表示用のテキスト
+	private GameObject scoreText;
+
+	//得点加算用の変数
+	private int score = 0;
+
+
+	// Use this for initialization
+	void Start () {
+		//シーン中のGameOverTextオブジェクトを取得
+		this.gameoverText = GameObject.Find("GameOverText");
+		this.scoreText = GameObject.Find("ScoreText");
+
+			}
+	
+	// Update is called once per frame
+	void Update () {
+		//ボールが画面外に出た場合
+		if (this.transform.position.z < this.visiblePosZ) {
+			//GameOverTextにゲームオーバーを表示
+			this.gameoverText.GetComponent<Text> ().text = "Game Over";
+		}  
+	}
+	
+		//ボールの衝突判定
+	void OnCollisionEnter(Collision other) {
+			//衝突判定
+			if (other.gameObject.tag == "SmallCloudTag") {
+			//スコア処理を追加
+
+			score += 10;
+			scoreText.GetComponent<Text>().text = score.ToString();
+			} else if(other.gameObject.tag == "LargeCloudTag") {
+	
+			score +=20;
+			scoreText.GetComponent<Text>().text = score.ToString();
+			}
+		}
+}
